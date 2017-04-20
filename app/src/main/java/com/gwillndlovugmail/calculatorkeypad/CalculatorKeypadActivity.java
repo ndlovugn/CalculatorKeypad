98c4ac4c981d5532af8e601e074f8a7781346b77
AppDeveloper <AppDeveloper>
2017-04-20 15:35:09 +0100

    Initial commit

---
+++ app/src/main/java/com/gwillndlovugmail/calculatorkeypad/CalculatorKeypadActivity.java
@@ -0,0 +1,90 @@
+package com.gwillndlovugmail.calculatorkeypad;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class CalculatorKeypadActivity extends AppCompatActivity {
+
+    public class MainActivity extends Activity {
+
+    }
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button10;
+    Button button11;
+    Button button12;
+    Button button13;
+    Button button14;
+    Button button15;
+    Button button16;
+    Button button17;
+    Button button18;
+    Button button19;
+    Button button20;
+    Button button21;
+    Button button22;
+    Button button23;
+    Button button24;
+    Button button25;
+    Button button26;
+    Button button27;
+    Button button28;
+    Button button29;
+    Button button30;
+    Button button31;
+
+
+
+
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_calculator_keypad);{
+
+
+                button1 =(Button)findViewById(R.id.button1);
+                button2 =(Button)findViewById(R.id.button2);
+                button3 =(Button)findViewById(R.id.button3);
+                button4 =(Button)findViewById(R.id.button4);
+                button5 =(Button)findViewById(R.id.button5);
+                button6 =(Button)findViewById(R.id.button6);
+                button7 =(Button)findViewById(R.id.button7);
+                button8 =(Button)findViewById(R.id.button8);
+                button9 =(Button)findViewById(R.id.button9);
+
+                button11 =(Button)findViewById(R.id.button11);
+                button12 =(Button)findViewById(R.id.button12);
+                button13 =(Button)findViewById(R.id.button13);
+                button14 =(Button)findViewById(R.id.button14);
+                button15 =(Button)findViewById(R.id.button15);
+                button16 =(Button)findViewById(R.id.button16);
+                button17 =(Button)findViewById(R.id.button17);
+                button18 =(Button)findViewById(R.id.button18);
+                button19 =(Button)findViewById(R.id.button19);
+                button20 =(Button)findViewById(R.id.button20);
+                button21 =(Button)findViewById(R.id.button21);
+                button22 =(Button)findViewById(R.id.button22);
+                button23 =(Button)findViewById(R.id.button23);
+                button24 =(Button)findViewById(R.id.button24);
+                button25 =(Button)findViewById(R.id.button25);
+
+                button27 =(Button)findViewById(R.id.button27);
+
+
+                button29 =(Button)findViewById(R.id.button29);
+                button30 =(Button)findViewById(R.id.button30);
+                button31 =(Button)findViewById(R.id.button31);
+
+            }
+        }
+}
---
+++ app/src/main/java/ie/corktrainingcentre/calculatorkeypad/CalculatorKeypad.java
@@ -0,0 +1,16 @@
+package ie.corktrainingcentre.calculatorkeypad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorKeypad extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_keypad);
+
+
+    }
+
+}
